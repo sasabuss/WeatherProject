a06cee4c99e58dc7b27b8b02307b5a2d1203f7e6
Sasa <sasabuss96@gmail.com>
2025-08-26 18:45:52 +0200

    *Added UserWeather Seeder

---
+++ database/seeders/UserWeatherSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\WeatherModel;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserWeatherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        $city = $this->command->ask('What is your city?');
+
+        if (empty($city)) {
+            $this->command->error('Please enter a city');
+            return;
+        }
+
+        $temperature = $this->command->ask('What is the temperature in city?');
+
+        if (empty($temperature)) {
+            $this->command->error('Please enter a temperature');
+            return;
+        }
+
+        WeatherModel::create([
+            'city' => $city,
+            'temperature' => $temperature,
+        ]);
+
+        $this->command->info("City {$city} with temperature {$temperature}°C added successfully!");
+    }
+
+
+}
