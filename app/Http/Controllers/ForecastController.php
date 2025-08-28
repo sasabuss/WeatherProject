4a5c78941bbd145cc307bd71120a2c220f06cde3
Sasa <sasabuss96@gmail.com>
2025-08-28 19:34:56 +0200

    *Added Cities model and migration

---
+++ app/Http/Controllers/ForecastController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ForecastController extends Controller
+{
+    public function index($city)
+    {
+        $forecasts = [
+            'Beograd' => [25,29,33,44,55],
+            'Sarajevo' =>[33,44,32,27,31]
+        ];
+
+        $city = strtolower($city);
+
+        if(!array_key_exists($city, $forecasts)) {
+            die('Ovaj grad ne postoji');
+        }
+    }
+}
